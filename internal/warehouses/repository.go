f16617993d9eb412e614f88764a8f740970c296b
brunobrolesi <bruno.brolesi@mercadolivre.com>
2022-06-03 14:09:38 -0300

    feat: add werehouses repository with create method

---
+++ internal/warehouses/repository.go
@@ -0,0 +1,28 @@
+package warehouses
+
+var ws []Warehouse = []Warehouse{}
+
+type Repository interface {
+	Create(warehouseCode string, address string, telephone string, minimumCapacity int, minimumTemperature float64) (Warehouse, error)
+}
+
+type repository struct{}
+
+func CreateRepository() Repository {
+	return &repository{}
+}
+
+func (r *repository) Create(warehouseCode string, address string, telephone string, minimumCapacity int, minimumTemperature float64) (Warehouse, error) {
+	w := Warehouse{
+		Id:                 1,
+		WarehouseCode:      warehouseCode,
+		Address:            address,
+		Telephone:          telephone,
+		MinimumCapacity:    minimumCapacity,
+		MinimumTemperature: minimumTemperature,
+	}
+
+	ws = append(ws, w)
+
+	return w, nil
+}
