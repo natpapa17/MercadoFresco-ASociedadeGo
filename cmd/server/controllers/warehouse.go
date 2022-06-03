6ec720a6add4efcb22f0eedc74031fd5f8f5cf5e
brunobrolesi <bruno.brolesi@mercadolivre.com>
2022-06-03 14:10:47 -0300

    feat: add createWareHouseController

--- cmd/server/controllers/warehouse.go
+++ cmd/server/controllers/warehouse.go
@@ -4,10 +4,50 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/natpapa17/MercadoFresco-ASociedadeGo/internal/warehouses"
 )
+
+type WarehouseController struct {
+	service warehouses.Service
+}
 
 func Ping(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"value": "ok",
 	})
+}
+
+func CreateWarehouseController(ws warehouses.Service) *WarehouseController {
+	return &WarehouseController{
+		service: ws,
+	}
+}
+
+func (wc *WarehouseController) CreateWarehouse(ctx *gin.Context) {
+	var req createWarehouseRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"message": "Invalid inputs",
+		})
+	}
+
+	w, err := wc.service.Create(req.WarehouseCode, req.Address, req.Telephone, req.MinimumCapacity, req.MinimumTemperature)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": "internal server error",
+		})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{
+		"data": w,
+	})
+}
+
+type createWarehouseRequest struct {
+	WarehouseCode      string  `json:"warehouse_code" binding:"required"`
+	Address            string  `json:"address" binding:"required"`
+	Telephone          string  `json:"telephone" binding:"required"`
+	MinimumCapacity    int     `json:"minimum_capacity" binding:"required"`
+	MinimumTemperature float64 `json:"minimum_temperature" binding:"required"`
 }
--- cmd/server/routes/router.go
+++ cmd/server/routes/router.go
@@ -3,14 +3,21 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/natpapa17/MercadoFresco-ASociedadeGo/cmd/server/controllers"
+	"github.com/natpapa17/MercadoFresco-ASociedadeGo/internal/warehouses"
 )
 
 func ConfigRoutes(r *gin.Engine) *gin.Engine {
+
+	wr := warehouses.CreateRepository()
+	ws := warehouses.CreateService(wr)
+	wc := controllers.CreateWarehouseController(ws)
+
 	mux := r.Group("api/")
 	{
 		warehouse := mux.Group("warehouse")
 		{
 			warehouse.GET("/", controllers.Ping)
+			warehouse.POST("/", wc.CreateWarehouse)
 		}
 	}
 
