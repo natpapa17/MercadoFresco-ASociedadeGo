16828fcfd420b1ff21cbc0875a9611b80c80aba8
Viviansol <viviansan93@gmail.com>
2022-06-03 18:33:33 -0300

    feat: seller router
    seller router

--- cmd/server/routes/router.go
+++ cmd/server/routes/router.go
@@ -3,15 +3,30 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/natpapa17/MercadoFresco-ASociedadeGo/cmd/server/controllers"
+	"github.com/natpapa17/MercadoFresco-ASociedadeGo/internal/sellers"
+	"github.com/natpapa17/MercadoFresco-ASociedadeGo/pkg/store"
 )
 
 func ConfigRoutes(r *gin.Engine) *gin.Engine {
+	sellersDb := store.New(store.FileType, "../../../data/sellers.json")
+	sellerRepo := sellers.NewRepository(sellersDb)
+	sellerService := sellers.NewService(sellerRepo)
+	sellerControllers := controllers.NewSeller(sellerService)
+	
 	mux := r.Group("api/")
 	{
 		warehouse := mux.Group("warehouse")
 		{
 			warehouse.GET("/", controllers.Ping)
 		}
+		seller := mux.Group("seller")
+	{
+		seller.GET("/", sellerControllers.GetAll())
+		seller.POST("/", sellerControllers.Store())
+		seller.DELETE("/:id", sellerControllers.Delete())
+		seller.PATCH("/:id", sellerControllers.Update())
+		
+	}
 	}
 
 	return r
