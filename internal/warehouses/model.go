28fec31f463e6ef1f4f7a3ef18a3485ac7f6f106
brunobrolesi <bruno.brolesi@mercadolivre.com>
2022-06-03 23:22:53 -0300

    fix: json warehouse field names

--- internal/warehouses/model.go
+++ internal/warehouses/model.go
@@ -1,10 +1,10 @@
 package warehouses
 
 type Warehouse struct {
-	Id                 int
-	WarehouseCode      string
-	Address            string
-	Telephone          string
-	MinimumCapacity    int
-	MinimumTemperature float64
+	Id                 int     `json:"id"`
+	WarehouseCode      string  `json:"warehouse_code"`
+	Address            string  `json:"address"`
+	Telephone          string  `json:"telephone"`
+	MinimumCapacity    int     `json:"minimum_capacity"`
+	MinimumTemperature float64 `json:"minimum_temperature"`
 }
