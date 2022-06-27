a05326a891266102548faba1122194f4b7dd13e8
brunobrolesi <bruno.brolesi@mercadolivre.com>
2022-06-27 17:49:37 -0300

    feat: add docker into project

---
+++ db/dump.sql
@@ -0,0 +1,13 @@
+USE `fresh_market`;
+
+CREATE TABLE warehouses (
+  id int not null auto_increment,
+  warehouse_code varchar(25) not null unique ,
+  address varchar(255) not null ,
+  telephone varchar(15) not null ,
+  minimum_capacity int not null ,
+  minimum_temperature int not null ,
+  primary key(id)
+);
+
+INSERT INTO `warehouses` (warehouse_code, address, telephone, minimum_capacity, minimum_temperature) VALUES ('XPTO', 'Rua Brasil, 123', '(44) 99999-9999', 10, 5);
