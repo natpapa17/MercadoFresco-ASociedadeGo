d814e35e1806025b92d79f7f503133e9e6e0d18f
brunobrolesi <bruno.brolesi@mercadolivre.com>
2022-06-28 18:08:03 -0300

    fix: temperature type

--- db/dump.sql
+++ db/dump.sql
@@ -6,8 +6,8 @@ CREATE TABLE warehouses (
   address varchar(255) not null ,
   telephone varchar(15) not null ,
   minimum_capacity int not null ,
-  minimum_temperature int not null ,
+  minimum_temperature float not null ,
   primary key(id)
 );
 
-INSERT INTO `warehouses` (warehouse_code, address, telephone, minimum_capacity, minimum_temperature) VALUES ('XPTO', 'Rua Brasil, 123', '(44) 99999-9999', 10, 5);
+INSERT INTO `warehouses` (warehouse_code, address, telephone, minimum_capacity, minimum_temperature) VALUES ('XPTO', 'Rua Brasil, 123', '(44) 99999-9999', 10, 5.4);
